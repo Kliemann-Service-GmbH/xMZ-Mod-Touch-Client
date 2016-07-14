e8a8f7b59bbd8d2e9f91412c88a446ab51d30a5f
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-14 12:02:05 +0200

    ServerCommand::Server implementiert

--- src/cmd.rs
+++ src/cmd.rs
@@ -4,9 +4,7 @@ use clap::{Arg, App, SubCommand};
 ///
 /// Dieser wird dann an den Server gesendet. Der wiederum parst dann diesen.
 pub fn read_command() -> String {
-    let mut command = "";
-    let mut subcommand = "";
-    let mut options: String = String::new();
+    let mut fullcommand: String = String::new();   // Return Value
 
     // Pull version information out of Cargo.toml
     let version = format!("{}.{}.{}{}",
@@ -58,68 +56,117 @@ pub fn read_command() -> String {
                 (@arg num: +takes_value +required "Relais Nummer(n)")
             )
         )
+        (@subcommand server =>
+            (about: "Konfiguration des Servers Lesen und Ändern")
+            (@subcommand set =>
+                (about: "Konfiguration des Servers (1. Parameter) durch den Wert (2. Parameter) ersetzen")
+                (@arg config_entry: +takes_value +required "Konfigurations Parameter")
+                (@arg value: +takes_value +required "zu setzender Wert")
+            )
+            (@subcommand get =>
+                (about: "Konfigurations Parameter abfragen")
+                (@arg config_entry: +takes_value +required "Konfigurations Parameter")
+            )
+        )
         (@subcommand module =>
             (about: "Konfiguration und Information der Sensor Module")
         )
     ).get_matches();
 
+    // LED Command
     if let Some(ref matches) = matches.subcommand_matches("led") {
-        command = "led";
+        let command = "led";
+        let mut subcommand = "";
+        let mut options = "";
+
         if let Some(ref matches) = matches.subcommand_matches("set") {
             subcommand = "set";
             if let Some(ref matches) = matches.value_of("num") {
-                options = matches.to_string();
+                options = matches;
             }
         }
         if let Some(ref matches) = matches.subcommand_matches("get") {
             subcommand = "get";
             if let Some(ref matches) = matches.value_of("num") {
-                options = matches.to_string();
+                options = matches;
             }
         }
         if let Some(ref matches) = matches.subcommand_matches("clear") {
             subcommand = "clear";
             if let Some(ref matches) = matches.value_of("num") {
-                options = matches.to_string();
+                options = matches;
             }
         }
         if let Some(ref matches) = matches.subcommand_matches("toggle") {
             subcommand = "toggle";
             if let Some(ref matches) = matches.value_of("num") {
-                options = matches.to_string();
+                options = matches;
             }
         }
+        fullcommand = format!("{} {} {}", command, subcommand, options);
     }
 
+    // Relais Command
     if let Some(ref matches) = matches.subcommand_matches("relais") {
-        command = "relais";
+        let command = "relais";
+        let mut subcommand = "";
+        let mut options = "";
+
         if let Some(ref matches) = matches.subcommand_matches("set") {
             subcommand = "set";
             if let Some(ref matches) = matches.value_of("num") {
-                options = matches.to_string();
+                options = matches;
             }
         }
         if let Some(ref matches) = matches.subcommand_matches("get") {
             subcommand = "get";
             if let Some(ref matches) = matches.value_of("num") {
-                options = matches.to_string();
+                options = matches;
             }
         }
         if let Some(ref matches) = matches.subcommand_matches("clear") {
             subcommand = "clear";
             if let Some(ref matches) = matches.value_of("num") {
-                options = matches.to_string();
+                options = matches;
             }
         }
         if let Some(ref matches) = matches.subcommand_matches("toggle") {
             subcommand = "toggle";
             if let Some(ref matches) = matches.value_of("num") {
-                options = matches.to_string();
+                options = matches;
             }
         }
+        fullcommand = format!("{} {} {}", command, subcommand, options);
     }
 
-    format!("{} {} {}", command, subcommand, options)
+    // Server Command
+    if let Some(ref matches) = matches.subcommand_matches("server") {
+        let command = "server";
+        let mut subcommand = "";
+        let mut config_entry = "";
+        let mut value = "";
 
+        if let Some(ref matches) = matches.subcommand_matches("set") {
+            subcommand = "set";
+            if let Some(ref matches) = matches.value_of("config_entry") {
+                config_entry = matches;
+            }
+            if let Some(ref matches) = matches.value_of("value") {
+                value = matches;
+            }
+        }
+        fullcommand = format!("{} {} {} {}", command, subcommand, config_entry, value);
+
+        if let Some(ref matches) = matches.subcommand_matches("get") {
+            subcommand = "get";
+            if let Some(ref matches) = matches.value_of("config_entry") {
+                config_entry = matches;
+            }
+        }
+        fullcommand = format!("{} {} {}", command, subcommand, config_entry);
+    }
+
+    // Return value senden
+    fullcommand
 
 }
